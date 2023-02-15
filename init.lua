20de568d50403b47e92d4f0698ed572face1d81e
Tom Taylor <tom@planccck.xyz>
2023-02-15 19:59:47 +0000

    Stylua

--- init.lua
+++ init.lua
@@ -5,27 +5,27 @@ vim.cmd([[
 ]])
 vim.opt.relativenumber = true
 vim.opt.termguicolors = true
-vim.opt.tabstop=4
-vim.opt.shiftwidth=4
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-vim.opt.softtabstop=4
+vim.opt.softtabstop = 4
 vim.opt.modeline = true
 vim.opt.ruler = true
-vim.opt.backspace="2"
-vim.opt.encoding="utf-8"
+vim.opt.backspace = "2"
+vim.opt.encoding = "utf-8"
 vim.opt.spell = false
-vim.opt.spelllang="en"
-vim.opt.clipboard="unnamedplus"
-vim.opt.inccommand="split"
-vim.opt.winwidth=80
-vim.opt.list=true
+vim.opt.spelllang = "en"
+vim.opt.clipboard = "unnamedplus"
+vim.opt.inccommand = "split"
+vim.opt.winwidth = 80
+vim.opt.list = true
 
 HOME = os.getenv("HOME")
 
-vim.opt.undofile=true
-vim.opt.undodir=HOME.."/.local/share/nvim/undo"
-vim.opt.undolevels=1000
-vim.opt.undoreload=10000
+vim.opt.undofile = true
+vim.opt.undodir = HOME .. "/.local/share/nvim/undo"
+vim.opt.undolevels = 1000
+vim.opt.undoreload = 10000
 
 vim.opt.hidden = true
 vim.opt.wildmenu = true
@@ -34,20 +34,20 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.autoindent = true
 vim.opt.startofline = false
-vim.opt.laststatus=2
+vim.opt.laststatus = 2
 vim.opt.confirm = true
-vim.opt.cmdheight=1
+vim.opt.cmdheight = 1
 vim.opt.timeout = false
 vim.opt.ttimeout = true
-vim.opt.ttimeoutlen=200
-vim.opt.signcolumn="yes"
+vim.opt.ttimeoutlen = 200
+vim.opt.signcolumn = "yes"
 
-vim.opt.joinspaces=false
+vim.opt.joinspaces = false
 
-vim.opt.updatetime=300
-vim.opt.shortmess:append({c = true})
+vim.opt.updatetime = 300
+vim.opt.shortmess:append({ c = true })
 
-vim.opt.mouse="a"
+vim.opt.mouse = "a"
 
 vim.cmd([[
 set t_Co=256
@@ -64,15 +64,15 @@ vim.g.python3_host_prog = "python3"
 
 -- mappings
 function map(mode, shortcut, command)
-  vim.keymap.set(mode, shortcut, command, { noremap = true })
+	vim.keymap.set(mode, shortcut, command, { noremap = true })
 end
 
 function nmap(shortcut, command)
-  map('n', shortcut, command)
+	map("n", shortcut, command)
 end
 
 function imap(shortcut, command)
-  map('i', shortcut, command)
+	map("i", shortcut, command)
 end
 
 map("", "Y", "y$")
@@ -94,16 +94,14 @@ vim.g.maplocalleader = " "
 -- function + mappings
 
 function qf_toggle()
-  for i = 1,vim.fn.winnr('$'),1
-  do
-    bnum = vim.fn.winbufnr(i)
-    if vim.fn.getbufvar(bnum, '&buftype') == 'quickfix'
-      then
-      vim.cmd.cclose()
-    else
-      vim.cmd.copen()
-    end
-  end
+	for i = 1, vim.fn.winnr("$"), 1 do
+		bnum = vim.fn.winbufnr(i)
+		if vim.fn.getbufvar(bnum, "&buftype") == "quickfix" then
+			vim.cmd.cclose()
+		else
+			vim.cmd.copen()
+		end
+	end
 end
 
 map("n", "yoq", qf_toggle)
@@ -136,24 +134,22 @@ map("n", "<leader>ss ;call", "LoadSession()<cr>")
 map("n", "<leader>sS ;call", "MakeSession()<cr>")
 map("n", "<C-L> :nohl<CR><C-L>:syntax sync", "minlines=10000<cr>")
 
+local remember_folds = vim.api.nvim_create_augroup("remember_folds", { clear = true })
+vim.api.nvim_create_autocmd({ "BufWinLeave" }, {
+	pattern = "*",
+	group = remember_folds,
+	command = "silent! mkview",
+})
+vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+	pattern = "*",
+	group = remember_folds,
+	command = "silent! loadview",
+})
 
-local remember_folds = vim.api.nvim_create_augroup('remember_folds', { clear = true })
-vim.api.nvim_create_autocmd({ 'BufWinLeave' }, {
-    pattern = '*',
-    group = remember_folds,
-    command = 'silent! mkview'
-  })
-vim.api.nvim_create_autocmd({ 'BufWinEnter' }, {
-    pattern = '*',
-    group = remember_folds,
-    command = 'silent! loadview'
-  })
+require("plugins")
 
-require('plugins')
-
-if(vim.fn.has('termguicolors'))
-  then
-  vim.opt.termguicolors = true
+if vim.fn.has("termguicolors") then
+	vim.opt.termguicolors = true
 end
 
 vim.opt.background = "dark"
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,65 +1,64 @@
-return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+return require("packer").startup(function(use)
+	-- Packer can manage itself
+	use("wbthomason/packer.nvim")
 
-  -- Load on an autocommand event
-  use {'andymass/vim-matchup', event = 'VimEnter'}
+	-- Load on an autocommand event
+	use({ "andymass/vim-matchup", event = "VimEnter" })
 
-  use 'sainnhe/gruvbox-material'
+	use("sainnhe/gruvbox-material")
 
-  -- ansi color esc
-  use 'chrisbra/Colorizer'
+	-- ansi color esc
+	use("chrisbra/Colorizer")
 
-  -- Brackets
-  use 'machakann/vim-sandwich'
+	-- Brackets
+	use("machakann/vim-sandwich")
 
-  -- Fancy git stuff
-  use 'tpope/vim-fugitive'
+	-- Fancy git stuff
+	use("tpope/vim-fugitive")
 
-  -- fzf
-  use 'junegunn/fzf'
-  use 'junegunn/fzf.vim'
+	-- fzf
+	use("junegunn/fzf")
+	use("junegunn/fzf.vim")
 
-  -- More verbs/ motions
-  use 'wellle/targets.vim'
-  use 'easymotion/vim-easymotion'
+	-- More verbs/ motions
+	use("wellle/targets.vim")
+	use("easymotion/vim-easymotion")
 
-  -- syntax
-  use 'sheerun/vim-polyglot'
+	-- syntax
+	use("sheerun/vim-polyglot")
 
-  -- common bindings
-  use 'tpope/vim-unimpaired'
+	-- common bindings
+	use("tpope/vim-unimpaired")
 
-  -- rename
-  use 'tpope/vim-eunuch'
+	-- rename
+	use("tpope/vim-eunuch")
 
-  -- Async
-  use 'mhinz/neovim-remote'
+	-- Async
+	use("mhinz/neovim-remote")
 
-  -- Async make
-  use 'neomake/neomake'
+	-- Async make
+	use("neomake/neomake")
 
-  -- Unicode
-  use 'chrisbra/unicode.vim'
+	-- Unicode
+	use("chrisbra/unicode.vim")
 
-  -- Makestuff
-  use 'tpope/vim-dispatch'
-  -- Math
-  use 'arecarn/vim-crunch'
-  -- Comment
-  use 'tpope/vim-commentary'
+	-- Makestuff
+	use("tpope/vim-dispatch")
+	-- Math
+	use("arecarn/vim-crunch")
+	-- Comment
+	use("tpope/vim-commentary")
 
-  -- GO
-  use 'fatih/vim-go'
+	-- GO
+	use("fatih/vim-go")
 
-  -- swap
-  use 'machakann/vim-swap'
+	-- swap
+	use("machakann/vim-swap")
 
-  -- indent markers
-  use 'Yggdroot/indentLine'
+	-- indent markers
+	use("Yggdroot/indentLine")
 
-  -- Spell check
-  use 'kamykn/spelunker.vim'
-  use 'kamykn/popup-menu.nvim'
-
+	-- Spell check
+	use("kamykn/spelunker.vim")
+	use("kamykn/popup-menu.nvim")
 end)
