2655606ccca40b1cf72b9d34dbf49bed5decfb64
t-taylor <thomas.taylor@infinity.co>
2023-02-28 09:30:21 +0000

    diagas

--- plugin/after/null-ls.nvim.lua
+++ plugin/after/null-ls.nvim.lua
@@ -3,8 +3,8 @@ local null_ls = require("null-ls")
 null_ls.setup({
 	sources = {
 		null_ls.builtins.formatting.stylua,
-		null_ls.builtins.diagnostics.cspell,
-		null_ls.builtins.code_actions.cspell,
+		null_ls.builtins.diagnostics.codespell,
+		null_ls.builtins.formatting.goimports,
 		null_ls.builtins.formatting.goimports_reviser,
 		null_ls.builtins.diagnostics.golangci_lint,
 		null_ls.builtins.formatting.gofumpt,
