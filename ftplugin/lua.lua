e57343fc32e952501f7da8ec1c658949c0dd2483
t-taylor <thomas.taylor@infinity.co>
2023-03-02 16:38:39 +0000

    Remove bad autocmd

--- ftplugin/lua.lua
+++ ftplugin/lua.lua
@@ -2,10 +2,3 @@ require("lspconfig")["lua_ls"].setup({
 	on_attach = lsp_on_attach,
 	flags = lsp_flags,
 })
-
-vim.cmd([[
-augroup stylua_save
-  autocmd!
-  autocmd BufWritePost *.lua silent Dispatch! stylua %
-augroup end
-]])
