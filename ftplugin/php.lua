fafd30862b1b65cb5e343268909b6021578ac224
t-taylor <thomas.taylor@infinity.co>
2023-03-03 10:18:21 +0000

    php

--- ftplugin/php.lua
+++ ftplugin/php.lua
@@ -3,3 +3,9 @@ vim.cmd([[
     syntax sync maxlines=240
     set synmaxcol=800
 ]])
+
+require("lspconfig")["phpactor"].setup({
+    on_attach = lsp_on_attach,
+    flags = lsp_flags,
+    settings = {},
+})
--- plugin/after/null-ls.nvim.lua
+++ plugin/after/null-ls.nvim.lua
@@ -1,26 +1,26 @@
 local null_ls = require("null-ls")
 
 null_ls.setup({
-	sources = {
-		-- lua
-		null_ls.builtins.formatting.stylua,
-		-- golang
-		null_ls.builtins.formatting.goimports,
-		null_ls.builtins.formatting.goimports_reviser,
-		null_ls.builtins.diagnostics.golangci_lint,
-		null_ls.builtins.formatting.gofumpt,
-		--null_ls.builtins.diagnostics.revive,
-		-- php
-		--null_ls.builtins.diagnostics.phpcs,
-		null_ls.builtins.diagnostics.phpmd,
-		--null_ls.builtins.diagnostics.phpstan,
-		-- terraform
-		null_ls.builtins.diagnostics.terraform_validate,
-		null_ls.builtins.formatting.terraform_fmt,
-		-- misc
-		null_ls.builtins.diagnostics.semgrep,
-		null_ls.builtins.diagnostics.codespell,
-	},
+    sources = {
+        -- lua
+        null_ls.builtins.formatting.stylua,
+        -- golang
+        null_ls.builtins.formatting.goimports,
+        null_ls.builtins.formatting.goimports_reviser,
+        null_ls.builtins.diagnostics.golangci_lint,
+        null_ls.builtins.formatting.gofumpt,
+        --null_ls.builtins.diagnostics.revive,
+        -- php
+        --null_ls.builtins.diagnostics.phpcs,
+        --null_ls.builtins.diagnostics.phpmd,
+        null_ls.builtins.diagnostics.phpstan,
+        -- terraform
+        null_ls.builtins.diagnostics.terraform_validate,
+        null_ls.builtins.formatting.terraform_fmt,
+        -- misc
+        null_ls.builtins.diagnostics.semgrep,
+        null_ls.builtins.diagnostics.codespell,
+    },
 })
 
 -- Create some helpers
@@ -29,9 +29,9 @@ local acmd = vim.api.nvim_create_autocmd
 
 local _general = agrp("_general", { clear = true })
 acmd({ "BufWritePre" }, {
-	pattern = "*",
-	callback = function()
-		vim.lsp.buf.format()
-	end,
-	group = _general,
+    pattern = "*",
+    callback = function()
+        vim.lsp.buf.format()
+    end,
+    group = _general,
 })
