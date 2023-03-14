df274c72d951b96ccdc4be3353de253e5a14343b
t-taylor <thomas.taylor@infinity.co>
2023-03-14 09:28:22 +0000

    project and null

--- plugin/after/null-ls.nvim.lua
+++ plugin/after/null-ls.nvim.lua
@@ -1,36 +1,37 @@
 local null_ls = require("null-ls")
 
 null_ls.setup({
-    sources = {
-        -- lua
-        null_ls.builtins.formatting.stylua,
+	sources = {
+		-- lua
+		null_ls.builtins.formatting.stylua,
 
-        -- golang
-        null_ls.builtins.formatting.goimports,
-        null_ls.builtins.formatting.goimports_reviser,
-        null_ls.builtins.diagnostics.golangci_lint,
-        null_ls.builtins.formatting.gofumpt,
-        --null_ls.builtins.diagnostics.revive,
+		-- golang
+		null_ls.builtins.formatting.goimports,
+		null_ls.builtins.formatting.goimports_reviser,
+		null_ls.builtins.diagnostics.golangci_lint,
+		null_ls.builtins.formatting.gofumpt,
+		null_ls.builtins.diagnostics.staticcheck,
+		--null_ls.builtins.diagnostics.revive,
 
-        -- php
-        --null_ls.builtins.diagnostics.phpcs,
-        null_ls.builtins.diagnostics.phpmd.with({
-            extra_args = { "unusedcode" },
-        }),
-        --null_ls.builtins.diagnostics.phpstan,
-        --null_ls.builtins.diagnostics.php,
+		-- php
+		--null_ls.builtins.diagnostics.phpcs,
+		null_ls.builtins.diagnostics.phpmd.with({
+			extra_args = { "unusedcode" },
+		}),
+		--null_ls.builtins.diagnostics.phpstan,
+		--null_ls.builtins.diagnostics.php,
 
-        -- terraform
-        -- null_ls.builtins.diagnostics.terraform_validate,
-        null_ls.builtins.formatting.terraform_fmt,
+		-- terraform
+		-- null_ls.builtins.diagnostics.terraform_validate,
+		null_ls.builtins.formatting.terraform_fmt,
 
-        -- misc
-        null_ls.builtins.diagnostics.semgrep,
-        null_ls.builtins.diagnostics.codespell,
+		-- misc
+		--null_ls.builtins.diagnostics.semgrep,
+		null_ls.builtins.diagnostics.codespell,
 
-        -- javascript/typescript
-        null_ls.builtins.diagnostics.eslint,
-    },
+		-- javascript/typescript
+		null_ls.builtins.diagnostics.eslint,
+	},
 })
 
 -- Create some helpers
@@ -39,9 +40,9 @@ local acmd = vim.api.nvim_create_autocmd
 
 local _general = agrp("_general", { clear = true })
 acmd({ "BufWritePre" }, {
-    pattern = "*",
-    callback = function()
-        vim.lsp.buf.format()
-    end,
-    group = _general,
+	pattern = "*",
+	callback = function()
+		vim.lsp.buf.format()
+	end,
+	group = _general,
 })
